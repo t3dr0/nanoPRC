929297eea2a9c0e43e6352d8aa9aaa5c154c34b4
Michael Vrhel <michael@mvrhel.com>
2026-05-26 22:38:49 -0700

    Normalize AGPL license headers to nanoPRC naming

--- src/prc_internal_proto_api.h
+++ src/prc_internal_proto_api.h
@@ -1,17 +1,17 @@
 /* Copyright (C) 2023-2026 CascadiaVoxel LLC
 
-    nano_prc is free software: you can redistribute it and/or modify it under
+    nanoPRC is free software: you can redistribute it and/or modify it under
     the terms of the GNU Affero General Public License as published by the
     Free Software Foundation, either version 3 of the License, or (at your
     option) any later version.
 
-    nano_prc is distributed in the hope that it will be useful, but WITHOUT
+    nanoPRC is distributed in the hope that it will be useful, but WITHOUT
     ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
     FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
     License for more details.
 
     You should have received a copy of the GNU Affero General Public License
-    along with nano_prc. If not, see <https://www.gnu.org/licenses/>.
+    along with nanoPRC. If not, see <https://www.gnu.org/licenses/>.
 */
 
 #ifndef PRC_API_INTERNAL_PROTO_H
